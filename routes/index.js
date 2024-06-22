cf21dff2e119bfcab1a6be90abaaf42562369a86
Sidharth Bhasin <sid21bhasin@gmail.com>
2024-06-22 13:37:10 +0530

    fix: user should not go to login page it the user is already logged in

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const productModel = require('../models/product-model');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
-router.get('/', (req, res) => {
+
+router.get('/', async (req, res) => {
     let error = req.flash('error');
-    res.render('index', {error});
-})
+    if(error.length > 0){
+        return res.render('index', {error});
+    }
+    const token = req.cookies.token;
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_KEY);
+            if(decoded.email){
+                const products = await productModel.find();
+                return res.render('shop',{products});
+            } else {
+                return res.render('index');
+            }
+        } catch (error) {
+            return res.send( error.message);
+        }
+
+}})
 router.get('/shop', async (req, res) => {
     let products = await productModel.find();
     res.render('shop', {products});
